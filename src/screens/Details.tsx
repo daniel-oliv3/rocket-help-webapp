8e37b408c78bec80c300d5fb4b9669a05c58647b
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-03 13:42:26 -0300

    Update Details.tsx

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,10 +1,25 @@
-import { VStack } from 'native-base';
+import { VStack, Text } from 'native-base';
+import { useRoute } from '@react-navigation/native';
 import { Header } from '../components/Header';
 
+
+type RouteParams = {
+  orderId: string;
+}
+
+
+/* Detalhes */
 export function Details() {
+
+  const route = useRoute();
+  const { orderId } = route.params as RouteParams;
+
   return (
     <VStack flex={1} bg="gray.700">
         <Header title="Solicitação" />
+        <Text color="white">
+          {orderId}
+        </Text>
     </VStack>
   );
 }
