d0099ed965c69d1a4fb6485bfb0be034942269c4
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-03 10:14:57 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,19 @@
-import { HStack } from 'native-base';
+import { Heading, HStack, IconButton, useTheme, StyledProps } from 'native-base';
+import { CaretLeft } from 'phosphor-react-native';
 
+
+
+type Props = StyledProps & {
+  title: string;
+}
+
+
+/* Heading */
 export function Header() {
+
+  const { colors } = useTheme();
+
+
   return (
     <HStack
         w="full"
@@ -10,6 +23,14 @@ export function Header() {
         pb={6}
         pt={12}
     >
+
+      <IconButton 
+        icon={<CaretLeft color={colors.gray[200]} size={24} />}
+      />
+
+      <Heading>
+
+      </Heading>
 
     </HStack>
   );
